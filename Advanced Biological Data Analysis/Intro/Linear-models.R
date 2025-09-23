85031916f9057efdeafca9c379a6daa7d99788fe
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2025-09-23 13:52:47 +0200

    useful things

---
+++ Advanced Biological Data Analysis/Intro/Linear-models.R
@@ -0,0 +1,14 @@
+##%#########################################################################%##
+#                                                                             #
+#                       Linear Models - Zoja Manček Páli                      #
+#                              Date: 23.9.2025                                #
+#                                                                             #
+##%#########################################################################%##
+
+#WD
+setwd(setwd("/Users/zojamancekpali/Desktop/KU Leuven/Advanced Biological Data Analysis/Intro"))
+getwd()
+
+#packages
+
+#
