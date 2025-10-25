7cf19d86b5536908f151cf79f75799b9c3307280
zmancekpali <mancekpalizoja@gmail.com>
2025-10-25 17:46:37 +0200

    mini changes

--- Stress Ecology & Ecotoxicology/Report/SEE report.R
+++ Stress Ecology & Ecotoxicology/Report/SEE report.R
@@ -9,12 +9,20 @@ acute <- read.xlsx("Acutetox_2025.xlsx")
 head(chronic)
 str(chronic)
 
+acute <- acute %>% mutate(
+  temp_f = factor(temperature, levels = c(20,24), labels = c("20°C","24°C")),
+  zinc_f = factor(zinc, levels = c(0,1), labels = c("No Zn","Zn")),
+  total = alive + dead + immo,
+  prop_alive = alive / total)
+
 chronic <- chronic %>% rename(temp = temperature, 
                               h560 = "Absorbance(560nm)Haem",
                               h576 = "Absorbance(576nm)Haem",
                               h600 = "Absorbance(600nm)Haem",
                               h_nn = "Haemo.non.normalized",
-                              p593 = "Absorbance(593nm)Prot")
+                              p593 = "Absorbance(593nm)Prot") %>%  mutate(
+                                temp_f = factor(temp, levels = c(20,24), labels = c("20°C","24°C")),
+                                zinc_f = factor(zinc, levels = c(0,1), labels = c("No Zn","Zn")))
 
 head(acute)
 str(acute)
@@ -29,3 +37,6 @@ plot(zinc ~ h600, data = chronic)
 plot(zinc ~ p593, data = chronic)
 plot(zinc ~ hemo, data = chronic)
 plot(zinc ~ protein, data = chronic)
+
+
+#EC50
